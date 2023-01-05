14483428430d6c40b02a676f34d4007b3be0d45a
Javad <ja7ad@live.com>
2023-01-05 12:32:00 +0330

    update: client for zarinpal for request type

--- providers/zarinpal/zarinpal.go
+++ providers/zarinpal/zarinpal.go
@@ -9,44 +9,43 @@ import (
 )
 
 const (
-	ZARINPAL_REQUEST_API_ENDPOINT                = "https://api.zarinpal.com/pg/v4/payment/request.json"
-	ZARINPAL_VERIFY_API_ENDPOINT                 = "https://api.zarinpal.com/pg/v4/payment/verify.json"
-	ZARINPAL_UNVERIFIED_TRANSACTION_API_ENDPOINT = "https://api.zarinpal.com/pg/v4/payment/unVerified.json"
+	ZARINPAL_HOST         = "https://api.zarinpal.com"
+	ZARINPAL_SANDBOX_HOST = "https://sandbox.zarinpal.com"
+)
 
-	ZARINPAL_REQUEST_SANDBOX_API_ENDPOINT = "https://sandbox.zarinpal.com/pg/v4/payment/request.json"
-	ZARINPAL_VERIFY_SANDBOX_API_ENDPOINT  = "https://sandbox.zarinpal.com/pg/v4/payment/verify.json"
+const (
+	ZARINPAL_REQUEST_API_ENDPOINT                = "/pg/v4/payment/request.json"
+	ZARINPAL_VERIFY_API_ENDPOINT                 = "/pg/v4/payment/verify.json"
+	ZARINPAL_UNVERIFIED_TRANSACTION_API_ENDPOINT = "/pg/v4/payment/unVerified.json"
 )
 
 var _ Interface = (*Zarinpal)(nil)
 
 type Interface interface {
-	RequestPayment(ctx context.Context, amount int, callBackUrl, currency, description string, metaData map[string]interface{}) (*PaymentResponse, *status.Status)
-	VerifyPayment(ctx context.Context, amount int, authority string) (*VerifyResponse, *status.Status)
-	UnverifiedTransactions(ctx context.Context) (*UnverifiedTransactionsResponse, *status.Status)
-	FloatingShareSettlement(ctx context.Context, amount int, description, callbackUrl string, wages []*Wages, metaData map[string]interface{}) (*FloatingShareSettlementResponse, *status.Status)
-	VerifyFloatingShareSettlement(ctx context.Context, amount int, authority string) (*VerifyFloatingShareSettlementResponse, *status.Status)
+	RequestPayment(ctx context.Context, amount int, callBackUrl, currency, description string, metaData map[string]interface{}) (*PaymentResponse, error)
+	VerifyPayment(ctx context.Context, amount int, authority string) (*VerifyResponse, error)
+	UnverifiedTransactions(ctx context.Context) (*UnverifiedTransactionsResponse, error)
+	FloatingShareSettlement(ctx context.Context, amount int, description, callbackUrl string, wages []*Wages, metaData map[string]interface{}) (*FloatingShareSettlementResponse, error)
+	VerifyFloatingShareSettlement(ctx context.Context, amount int, authority string) (*VerifyFloatingShareSettlementResponse, error)
 }
 
 // New create zarinpal provider object for user factory request methods
-func New(client client.Transporter, merchantID string, sandbox bool) (Interface, *status.Status) {
+func New(client client.Transporter, merchantID string, sandbox bool) (Interface, error) {
 	if client == nil {
 		return nil, status.ERR_CLIENT_IS_NIL
 	}
 
-	client.GetMutex().Lock()
-	defer client.GetMutex().Unlock()
-
 	zarinpal := &Zarinpal{
 		client:             client,
 		merchantID:         merchantID,
+		baseUrl:            ZARINPAL_HOST,
 		requestEndpoint:    ZARINPAL_REQUEST_API_ENDPOINT,
 		verifyEndpoint:     ZARINPAL_VERIFY_API_ENDPOINT,
 		unverifiedEndpoint: ZARINPAL_UNVERIFIED_TRANSACTION_API_ENDPOINT,
 	}
 
 	if sandbox {
-		zarinpal.requestEndpoint = ZARINPAL_REQUEST_SANDBOX_API_ENDPOINT
-		zarinpal.verifyEndpoint = ZARINPAL_VERIFY_SANDBOX_API_ENDPOINT
+		zarinpal.baseUrl = ZARINPAL_SANDBOX_HOST
 	}
 
 	if err := client.GetValidator().Struct(zarinpal); err != nil {
@@ -57,7 +56,7 @@ func New(client client.Transporter, merchantID string, sandbox bool) (Interface,
 }
 
 // RequestPayment create payment request and return status code and authority
-func (z *Zarinpal) RequestPayment(ctx context.Context, amount int, callBackUrl, currency, description string, metaData map[string]interface{}) (*PaymentResponse, *status.Status) {
+func (z *Zarinpal) RequestPayment(ctx context.Context, amount int, callBackUrl, currency, description string, metaData map[string]interface{}) (*PaymentResponse, error) {
 	req := &paymentRequest{
 		MerchantID:  z.merchantID,
 		Amount:      amount,
@@ -71,19 +70,21 @@ func (z *Zarinpal) RequestPayment(ctx context.Context, amount int, callBackUrl, 
 		return nil, status.New(0, http.StatusBadRequest, codes.InvalidArgument, err.Error())
 	}
 
-	resp := &PaymentResponse{}
-	s := z.client.Request(ctx, z.requestEndpoint, "", client.POST, map[string]string{}, req, resp)
-	if s.HttpStatusCode == http.StatusInternalServerError {
-		return nil, s
+	response := &PaymentResponse{}
+	resp, err := z.client.Post(ctx, &client.APIConfig{Host: z.baseUrl, Path: z.requestEndpoint}, nil, req)
+	if err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
 	}
 
-	s.ProviderStatusCode = resp.Data.Code
+	if err := resp.GetJSON(response); err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
+	}
 
-	return resp, s
+	return response, nil
 }
 
 // VerifyPayment transaction by merchant id, amount and authority to payment provider
-func (z *Zarinpal) VerifyPayment(ctx context.Context, amount int, authority string) (*VerifyResponse, *status.Status) {
+func (z *Zarinpal) VerifyPayment(ctx context.Context, amount int, authority string) (*VerifyResponse, error) {
 	req := &verifyRequest{
 		MerchantID: z.merchantID,
 		Amount:     amount,
@@ -94,19 +95,21 @@ func (z *Zarinpal) VerifyPayment(ctx context.Context, amount int, authority stri
 		return nil, status.New(0, http.StatusBadRequest, codes.InvalidArgument, err.Error())
 	}
 
-	resp := &VerifyResponse{}
-	s := z.client.Request(ctx, z.requestEndpoint, "", client.POST, map[string]string{}, req, resp)
-	if s.HttpStatusCode == http.StatusInternalServerError {
-		return nil, s
+	response := &VerifyResponse{}
+	resp, err := z.client.Post(ctx, &client.APIConfig{Host: z.baseUrl, Path: z.verifyEndpoint}, nil, req)
+	if err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
 	}
 
-	s.ProviderStatusCode = resp.Data.Code
+	if err := resp.GetJSON(response); err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
+	}
 
-	return resp, s
+	return response, nil
 }
 
 // UnverifiedTransactions get unverified transactions from provider
-func (z *Zarinpal) UnverifiedTransactions(ctx context.Context) (*UnverifiedTransactionsResponse, *status.Status) {
+func (z *Zarinpal) UnverifiedTransactions(ctx context.Context) (*UnverifiedTransactionsResponse, error) {
 	req := &unverifiedTransactionsRequest{
 		MerchantID: z.merchantID,
 	}
@@ -115,20 +118,22 @@ func (z *Zarinpal) UnverifiedTransactions(ctx context.Context) (*UnverifiedTrans
 		return nil, status.New(0, http.StatusBadRequest, codes.InvalidArgument, err.Error())
 	}
 
-	resp := &UnverifiedTransactionsResponse{}
-	s := z.client.Request(ctx, z.requestEndpoint, "", client.POST, map[string]string{}, req, resp)
-	if s.HttpStatusCode == http.StatusInternalServerError {
-		return nil, s
+	response := &UnverifiedTransactionsResponse{}
+	resp, err := z.client.Post(ctx, &client.APIConfig{Host: z.baseUrl, Path: z.unverifiedEndpoint}, nil, req)
+	if err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
 	}
 
-	s.ProviderStatusCode = resp.Data.Code
+	if err := resp.GetJSON(response); err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
+	}
 
-	return resp, s
+	return response, nil
 }
 
 // FloatingShareSettlement a special method is used for sellers who benefit from an incoming amount,
 // more information in https://docs.zarinpal.com/paymentGateway/setshare.html#%D8%AA%D8%B3%D9%88%DB%8C%D9%87-%D8%A7%D8%B4%D8%AA%D8%B1%D8%A7%DA%A9%DB%8C-%D8%B4%D9%86%D8%A7%D9%88%D8%B1
-func (z *Zarinpal) FloatingShareSettlement(ctx context.Context, amount int, description, callbackUrl string, wages []*Wages, metaData map[string]interface{}) (*FloatingShareSettlementResponse, *status.Status) {
+func (z *Zarinpal) FloatingShareSettlement(ctx context.Context, amount int, description, callbackUrl string, wages []*Wages, metaData map[string]interface{}) (*FloatingShareSettlementResponse, error) {
 	req := &floatingShareSettlementRequest{
 		MerchantID:  z.merchantID,
 		Amount:      amount,
@@ -142,17 +147,21 @@ func (z *Zarinpal) FloatingShareSettlement(ctx context.Context, amount int, desc
 		return nil, status.New(0, http.StatusBadRequest, codes.InvalidArgument, err.Error())
 	}
 
-	resp := &FloatingShareSettlementResponse{}
-	s := z.client.Request(ctx, z.requestEndpoint, "", client.POST, map[string]string{}, req, resp)
-	if s.HttpStatusCode == http.StatusInternalServerError {
-		return nil, s
+	response := &FloatingShareSettlementResponse{}
+	resp, err := z.client.Post(ctx, &client.APIConfig{Host: z.baseUrl, Path: z.requestEndpoint}, nil, req)
+	if err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
 	}
 
-	return resp, s
+	if err := resp.GetJSON(response); err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
+	}
+
+	return response, nil
 }
 
 // VerifyFloatingShareSettlement verify floating share settlement
-func (z *Zarinpal) VerifyFloatingShareSettlement(ctx context.Context, amount int, authority string) (*VerifyFloatingShareSettlementResponse, *status.Status) {
+func (z *Zarinpal) VerifyFloatingShareSettlement(ctx context.Context, amount int, authority string) (*VerifyFloatingShareSettlementResponse, error) {
 	req := &verifyRequest{
 		MerchantID: z.merchantID,
 		Amount:     amount,
@@ -163,13 +172,15 @@ func (z *Zarinpal) VerifyFloatingShareSettlement(ctx context.Context, amount int
 		return nil, status.New(0, http.StatusBadRequest, codes.InvalidArgument, err.Error())
 	}
 
-	resp := &VerifyFloatingShareSettlementResponse{}
-	s := z.client.Request(ctx, z.requestEndpoint, "", client.POST, map[string]string{}, req, resp)
-	if s.HttpStatusCode == http.StatusInternalServerError {
-		return nil, s
+	response := &VerifyFloatingShareSettlementResponse{}
+	resp, err := z.client.Post(ctx, &client.APIConfig{Host: z.baseUrl, Path: z.requestEndpoint}, nil, req)
+	if err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
 	}
 
-	s.ProviderStatusCode = resp.Data.Code
+	if err := resp.GetJSON(response); err != nil {
+		return nil, status.New(0, http.StatusInternalServerError, codes.Internal, err.Error())
+	}
 
-	return resp, s
+	return response, nil
 }
