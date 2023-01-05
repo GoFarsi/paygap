7c25e0255e2610e3de84419edb69c5d6cb43c22b
Javad <ja7ad@live.com>
2023-01-05 12:40:50 +0330

    add: zarinpal example usage

---
+++ _example/zarinpal/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/GoFarsi/pgp/client"
+	zarinpal "github.com/GoFarsi/pgp/providers/zarinpal/v4"
+	"log"
+)
+
+func main() {
+	c := client.New()
+	z, err := zarinpal.New(c, "YOUR_MERCHANT_ID", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := z.RequestPayment(context.Background(), 1000, "YOUR_CALL_BACK", "YOUR_CURRENCY", "description", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(resp)
+}
